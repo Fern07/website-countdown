1375fe3ed3edc3ae999161b148f84b9e0ff12f62
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-09-02 14:50:02 -0400

    time

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-const launchTime = new Date('12/29/2023').getTime()
+const launchTime = new Date('12/29/2023 10:00').getTime()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const currentTime = new Date().getTime()
-  const numberOfMilliseconds = parseInt(launchTime-currentTime)
+  const numberOfMilliseconds = parseInt(launchTime - currentTime)
   res.render(
     'index',
-     { title: 'PCHEEBUM', countDown: numberOfMilliseconds}
-     );
+     { title: 'PCHEEBUM', countDown: numberOfMilliseconds});
 });
 
 module.exports = router;
